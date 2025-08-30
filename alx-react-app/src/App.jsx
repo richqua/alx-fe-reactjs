c672d3b1a896ccee850f6783ef7b703a911419c5
Richard Quansah <rich.quansah03@gmail.com>
2025-08-30 21:32:29 +0000

    Added welcomeMessage file and exported to appjs

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import WelcomeMessage from './WelcomeMessage';
+import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,11 +25,12 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <h2>This was performed by Richard</h2>
+        <h3>This was performed by Richard</h3>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <WelcomeMessage />
     </>
   )
 }
