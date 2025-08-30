820aacf1dbcdce6000b25bddc2d77826c731339c
Richard Quansah <rich.quansah03@gmail.com>
2025-08-30 21:46:18 +0000

    Added components to src containing welcomeMessage

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg';
-import WelcomeMessage from './WelcomeMessage';
+import WelcomeMessage from './components/WelcomeMessage';
 import viteLogo from '/vite.svg';
 
 function App() {
