fbef6c9a2aff402922af35fb88b684d34fb9de03
Richard Quansah <rich.quansah03@gmail.com>
2025-09-07 02:09:06 +0000

    Improved web with inline css in react

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import Comments from './components/Comments';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
+import WelcomeMessage from './components/WelcomeMessage';
+import viteLogo from '/vite.svg';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+        <h3>This was performed by Richard</h3>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <div>
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+      <Comments
+      love="Benedictus"
+      time= "11pm"
+      issues="lying to girls" 
+      />
+    </div>
+    <div>
+      <comments
+      love="Benedictus"
+      time= "11pm"
+      issues="lying to girls" />
+    </div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ alx-react-app-new/src/components/Comments.jsx
@@ -0,0 +1,10 @@
+const comments=(props)=>{
+    return(
+        <div>
+            <p>love:{props.love}</p>
+            <p>time:{props.time}</p>
+            <p>issues:{props.issues}</p>
+        </div>
+    )
+}
+export default comments;
