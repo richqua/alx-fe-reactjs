4cd1ae3d9e717377d552ac438c2f65fa3d79098a
Richard Quansah <rich.quansah03@gmail.com>
2025-09-21 10:59:16 +0000

    Added new code line to app.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,7 +1,7 @@
 import Search from './components/Search';
 const App = () => {
   return (
-    <div style={{ padding: '3rem' }}>
+    <div style={{ padding:'3rem' }}>
       <h1 style={{ fontSize: '2rem', marginBottom: '1.5rem' }}>GitHub User Search</h1>
       <Search />
     </div>
